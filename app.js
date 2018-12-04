3f8a00fa2ada8c3f7dfa4961137d6c009942c20a
Daniel Lambert <test@lambert.com>
2018-12-04 08:54:41 -0500

    establish setup

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require("express");
+	app = express(),
+	bodyParser = require("body-parser"),
+
+
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+// index page 
+app.get('/', function(req, res) {
+    res.render('pages/index');
+});
+
+// about page 
+app.get('/about', function(req, res) {
+    res.render('pages/about');
+});
+
+
+var server = app.listen(3000, function (){
+  console.log("Calling app.listen's callback function.");
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
